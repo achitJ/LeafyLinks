35d9419e190487827dfee2653f50dedfd8a6a12c
achitj <ajain2801@gmail.com>
2023-08-16 22:41:53 +0530

    made changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<title>Social Seedlings</title>
+				<title>LeafyLinks</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
